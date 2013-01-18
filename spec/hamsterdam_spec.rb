756ef997ab40762c9f60ce9296aef1db5371c2ea
David Crosby <dcrosby42@gmail.com>
2013-01-18 09:32:45 -0500

    Restructure spec

--- spec/hamsterdam_spec.rb
+++ spec/hamsterdam_spec.rb
@@ -4,8 +4,10 @@ describe "Hamsterdam structures" do
   def define_hamsterdam_struct(*field_names)
     Hamsterdam::Struct.define(*field_names)
   end
+
+  let(:struct_class) { define_hamsterdam_struct(:top, :bottom) }
+
   describe "Struct.define" do
-    let(:struct_class) { define_hamsterdam_struct(:top, :bottom) }
 
     it "creates a structure class based on the given fields" do
       struct = struct_class.new(top: 200, bottom: "all the way down")
@@ -45,89 +47,89 @@ describe "Hamsterdam structures" do
     it "raises helpful error when constructed with invalid objects" do
       lambda do struct_class.new("LAWDY") end.should raise_error /Do not want.*LAWDY/
     end
-
-    describe "equality" do
-      it "considers two structs equal if they have the same field values" do
-        s1 = struct_class.new(top: 50, bottom: 75)
-        s2 = struct_class.new(top: 50, bottom: 75)
-        s1.eql?(s2).should == true
-        (s1 == s2).should == true
-        s1.should == s2
-      end
-
-      it "considers two structs NOT equal if they have the different field values" do
-        s1 = struct_class.new(top: 50, bottom: 75)
-        s2 = struct_class.new(top: 50, bottom: 74)
-        s3 = struct_class.new(top: 51, bottom: 75)
-
-        s1.eql?(s2).should_not == true
-        (s1 == s2).should_not == true
-        s1.should_not == s2
-
-        s1.eql?(s3).should_not == true
-        (s1 == s3).should_not == true
-        s1.should_not == s3
-      end
-
-      it "doesn't consider to structs eql? unless they are same class" do
-        s1 = struct_class.new(top: 50, bottom: 75)
-        s2 = define_hamsterdam_struct(:top, :bottom).new(top:50, bottom:75)
-        s1.eql?(s2).should == false
-        (s1 == s2).should == true # should still be ==
-      end
-
-      it "considers equal two structs if one has missing keys, and the other has nil values for those keys" do
-        s1 = struct_class.new(top: 50, bottom: nil)
-        s2 = struct_class.new(top: 50)
-        #binding.pry
-        s1.eql?(s2).should == true
-        (s1 == s2).should == true
-        s1.should == s2
-      end
-    end
-
-    it "uses the same #hash as Hamster::Hash" do
-      s1 = struct_class.new(top: 50, bottom: 75)
-      s1.hash.should == Hamster.hash(top:50, bottom:75).hash
-    end
-
-    describe "transformation" do
-      it "provides setters for individual fields that return an updated version of the struct" do
-        struct = struct_class.new(top: 10, bottom: 1)
-        struct2 = struct.set_top("woot")
-        struct2.top.should == "woot"
-        struct2.bottom.should == 1
-
-        struct.top.should == 10
-        struct.bottom.should == 1
-      end
-
-      it "provides a merge function" do
-        struct = struct_class.new(top: 10, bottom: 1)
-
-        struct2 = struct.merge(bottom: "new")
-        struct2.top.should == 10
-        struct2.bottom.should == "new"
-
-        struct.top.should == 10
-        struct.bottom.should == 1
-
-        struct3 = struct2.merge(top: "newer", bottom: "very")
-        struct3.top.should == "newer"
-        struct3.bottom.should == "very"
-      end
-
-      it "can merge-in Hamster::Hash" do
-        struct = struct_class.new(top: 10, bottom: 1)
-
-        struct2 = struct.merge(Hamster.hash(bottom: "newer", top: "newest"))
-        struct2.top.should == "newest"
-        struct2.bottom.should == "newer"
-
-        struct.top.should == 10
-        struct.bottom.should == 1
-      end
-    end
-
   end
+
+  describe "equality" do
+    it "considers two structs equal if they have the same field values" do
+      s1 = struct_class.new(top: 50, bottom: 75)
+      s2 = struct_class.new(top: 50, bottom: 75)
+      s1.eql?(s2).should == true
+      (s1 == s2).should == true
+      s1.should == s2
+    end
+
+    it "considers two structs NOT equal if they have the different field values" do
+      s1 = struct_class.new(top: 50, bottom: 75)
+      s2 = struct_class.new(top: 50, bottom: 74)
+      s3 = struct_class.new(top: 51, bottom: 75)
+
+      s1.eql?(s2).should_not == true
+      (s1 == s2).should_not == true
+      s1.should_not == s2
+
+      s1.eql?(s3).should_not == true
+      (s1 == s3).should_not == true
+      s1.should_not == s3
+    end
+
+    it "doesn't consider to structs eql? unless they are same class" do
+      s1 = struct_class.new(top: 50, bottom: 75)
+      s2 = define_hamsterdam_struct(:top, :bottom).new(top:50, bottom:75)
+      s1.eql?(s2).should == false
+      (s1 == s2).should == true # should still be ==
+    end
+
+    it "considers equal two structs if one has missing keys, and the other has nil values for those keys" do
+      s1 = struct_class.new(top: 50, bottom: nil)
+      s2 = struct_class.new(top: 50)
+      #binding.pry
+      s1.eql?(s2).should == true
+      (s1 == s2).should == true
+      s1.should == s2
+    end
+  end
+
+  it "uses the same #hash as Hamster::Hash" do
+    s1 = struct_class.new(top: 50, bottom: 75)
+    s1.hash.should == Hamster.hash(top:50, bottom:75).hash
+  end
+
+  describe "transformation" do
+    it "provides setters for individual fields that return an updated version of the struct" do
+      struct = struct_class.new(top: 10, bottom: 1)
+      struct2 = struct.set_top("woot")
+      struct2.top.should == "woot"
+      struct2.bottom.should == 1
+
+      struct.top.should == 10
+      struct.bottom.should == 1
+    end
+
+    it "provides a merge function" do
+      struct = struct_class.new(top: 10, bottom: 1)
+
+      struct2 = struct.merge(bottom: "new")
+      struct2.top.should == 10
+      struct2.bottom.should == "new"
+
+      struct.top.should == 10
+      struct.bottom.should == 1
+
+      struct3 = struct2.merge(top: "newer", bottom: "very")
+      struct3.top.should == "newer"
+      struct3.bottom.should == "very"
+    end
+
+    it "can merge-in Hamster::Hash" do
+      struct = struct_class.new(top: 10, bottom: 1)
+
+      struct2 = struct.merge(Hamster.hash(bottom: "newer", top: "newest"))
+      struct2.top.should == "newest"
+      struct2.bottom.should == "newer"
+
+      struct.top.should == 10
+      struct.bottom.should == 1
+    end
+  end
+
 end
